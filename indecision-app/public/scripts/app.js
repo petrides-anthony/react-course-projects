f28ac8d36f6da1974cbbcc40bbec3ae3ce64fe42
unknown <petrides.anthony@gmail.com>
2018-06-17 13:53:55 +0100

    indecision app initial commit

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('App.js is still running!');
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'h1',
+  null,
+  'This is JSX from app.js!'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is still running!');
+
+// JSX - JavaScript XML
+var template = <h1>This is JSX from app.js!</h1>;
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot);
