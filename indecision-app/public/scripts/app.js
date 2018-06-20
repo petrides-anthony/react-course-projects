0ddc570cd3b4eac0eb7a6211aa5e8957b624b0dd
Tony <tony@tonys-mbp.home>
2018-06-21 00:15:45 +0100

    lecture 12 progress

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,7 +4,8 @@ console.log('App.js is still running!');
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two']
 };
 
 var template = React.createElement(
@@ -15,10 +16,15 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     ),
     React.createElement(
         'ol',
@@ -39,8 +45,19 @@ var template = React.createElement(
 var user = {
     name: 'Tony',
     age: 26,
-    location: 'London'
+    location: 'Kensal Green'
 };
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 
 var templateTwo = React.createElement(
     'div',
@@ -48,20 +65,15 @@ var templateTwo = React.createElement(
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
